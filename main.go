50338abce10ac1c40c72567eed5422e5b0a899bf
Vadim Kosolapov <vadkos33@outlook.com>
2019-04-01 23:02:00 +0200

    Convert from and to RUB

--- main.go
+++ main.go
@@ -6,5 +6,6 @@ import (
 )
 
 func main() {
-	fmt.Println(src.Convert("USD", "AZN", 1))
+	fmt.Println(src.Convert("USD", "RUB", 1))
+	//fmt.Println(src.GetValuteByName("USD"))
 }
--- src/convert.go
+++ src/convert.go
@@ -4,7 +4,16 @@ import (
 	"log"
 )
 
-func Convert(from, to string, count int) (result float64, error string) {
+func Convert(from, to string, count int) (result float64) {
+	if from == "RUB"{
+		result := ConvertFromRubles(from, to, count)
+		return float64(int(result*100))/100
+	}
+	if to == "RUB"{
+		result := ConvertByRubles(from, to, count)
+		return float64(int(result*100))/100
+	}
+
 	valuteFromForOne, err := GetValuteByName(from)
 	valuteToForOne, err := GetValuteByName(to)
 	if err != nil {
@@ -12,10 +21,42 @@ func Convert(from, to string, count int) (result float64, error string) {
 		panic(err)
 	}
 	if valuteFromForOne == 0.0 || valuteToForOne == 0.0 {
-		return 0.0, "This currency not found, read the list of possible currencies"
+		return 0.0
 	}
 
 	transform := valuteFromForOne * float64(count) / valuteToForOne
 
-	return float64(int(transform*100))/100, "OK"
+	return float64(int(transform*100))/100
+}
+
+func ConvertByRubles(from, _ string, count int) (result float64) {
+	valuteFromForOne, err := GetValuteByName(from)
+	valuteToForOne := 1.0
+	if err != nil {
+		log.Printf(err.Error())
+		panic(err)
+	}
+	if valuteFromForOne == 0.0 {
+		return 0.0
+	}
+
+	transform := valuteFromForOne * float64(count) / valuteToForOne
+
+	return transform
+}
+
+func ConvertFromRubles(_, to string, count int) (result float64) {
+	valuteFromForOne := 1.0
+	valuteToForOne, err := GetValuteByName(to)
+	if err != nil {
+		log.Printf(err.Error())
+		panic(err)
+	}
+	if valuteToForOne == 0.0 {
+		return 0.0
+	}
+
+	transform := valuteFromForOne * float64(count) / valuteToForOne
+
+	return transform
 }
